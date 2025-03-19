731201b6bda804b56acd031138b3139c062046ab
un-caught <korodeleboluwatife@gmail.com>
2025-03-19 20:26:50 +0100

    Update settings.py

--- ptws/settings.py
+++ ptws/settings.py
@@ -78,12 +78,12 @@ WSGI_APPLICATION = 'ptws.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 # import dj_database_url
 
 # DATABASES = {
@@ -93,16 +93,16 @@ WSGI_APPLICATION = 'ptws.wsgi.application'
 # }
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'falcon_heig',
-        'USER' : 'falcon',
-        'PASSWORD': 'RPXJXVDvpwwNNAEmDxxOiJdoSBqgHj8z',
-        'HOST': 'dpg-cvcnnurv2p9s73ekc47g-a', 
-        'PORT': '5432',  
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'falcon_heig',
+#         'USER' : 'falcon',
+#         'PASSWORD': 'RPXJXVDvpwwNNAEmDxxOiJdoSBqgHj8z',
+#         'HOST': 'dpg-cvcnnurv2p9s73ekc47g-a', 
+#         'PORT': '5432',  
+#     }
+# }
 
 
 # Password validation
